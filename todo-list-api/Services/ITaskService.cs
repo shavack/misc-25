23f8d0d5d29f0c3d40d0bc00b7eada68429965bf
shavack <dkozuch@gmail.com>
2025-06-04 13:03:28 +0200

    Service instead of direct calls to DB

---
+++ todo-list-api/Services/ITaskService.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoListApi.Models;
+
+namespace TodoListApi.Services;
+public interface ITaskService
+{
+    Task<IEnumerable<TaskItem>> GetAllTasksAsync();
+    Task<TaskItem> GetTaskByIdAsync(int id);
+    Task<TaskItem> AddTaskAsync(TaskItem taskItem);
+    Task UpdateTaskAsync(int id, TaskItem taskItem);
+    Task DeleteTaskAsync(int id);
+}
